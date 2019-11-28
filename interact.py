96484847269242fd460b376b0ca5f98605ebd89e
anurag_pandey <anuragcarbon1998@gmail.com>
2019-11-28 09:49:00 +0530

    create Interact class and added progress bars

--- interact.py
+++ interact.py
@@ -8,92 +8,95 @@ import json
 import sys
 import datefinder
 from utils import extract_df
+from tqdm import tqdm 
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
-#better option would be to create a class
-def get_service():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle','rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json',SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle','wb') as token:
-            pickle.dump(creds,token)
+class Interact:
     
-    service = build('calendar','v3',credentials=creds)
-    return service
-
-def list_events(service):
-    now = datetime.utcnow().isoformat()+'Z'
-    print('Gettting the upcoming 10 events')
-    events_result = service.events().list(
-            calendarId='primary',timeMin=now,
-            maxResults=10,singleEvents=True,
-            orderBy='startTime').execute()
-    events = events_result.get('items',[])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime',event['start'].get('date'))
-        print(start,event['summary'])
-
-def insert_events(service,df):
-    eventIds = []
-    today = datetime.today()
-    start = today.weekday() % 6 + 1
-    #filling up one date completely first
-    for day in range(start,7):
-        for dtime in range(1,9): 
-            if df[day][dtime] == '':
-                    if 'break' in df[1][dtime].lower():
-                        summary = df[1][dtime]
-                    else:
-                        summary = df[day][dtime - 1]
+    def __init__(self,scopes):
+        self.scopes = scopes
+        self.get_service()
+        self.eventIds = [] 
+    
+    def get_service(self):
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle','rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
             else:
-                summary = df[day][dtime]
-            timing = df[0][dtime].split('to')
-            if timing[0].strip() >= '08:00' and timing[0].strip() < '12:00':
-                period = 'am'
-            else:
-                period = 'pm'
-            start_time = next(datefinder.find_dates(str(datetime.date(today)) + f' {timing[0]} {period}'))
-            if timing[1].strip() >= '08:00' and timing[1].strip() < '12:00':
-                period = 'am'
-            else:
-                period = 'pm'
-            end_time = next(datefinder.find_dates(str(datetime.date(today)) + f' {timing[1]} {period}'))
-            timezone = 'Asia/Kolkata'
-            event = {
-                'summary':summary,
-                'location':'PICT,Pune',
-                'start':{
-                    'dateTime': start_time.isoformat(), 
-                    'timeZone': timezone
-                },
-                'end':{
-                    'dateTime':end_time.isoformat(),
-                    'timeZone':timezone
+                flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json',self.scopes)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle','wb') as token:
+                pickle.dump(creds,token)
+        
+        self.service = build('calendar','v3',credentials=creds)
+
+    def list_events(self):
+        now = datetime.utcnow().isoformat()+'Z'
+        print('Gettting the upcoming 10 events')
+        events_result = self.service.events().list(
+                calendarId='primary',timeMin=now,
+                maxResults=10,singleEvents=True,
+                orderBy='startTime').execute()
+        events = events_result.get('items',[])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime',event['start'].get('date'))
+            print(start,event['summary'])
+
+    def insert_events(self,df):
+        today = datetime.today()
+        start = today.weekday() % 6 + 1
+        #filling up one date completely first
+        for day in tqdm(range(start,7),desc='Progress'):
+            for dtime in tqdm(range(1,9),desc=f'{today}'): 
+                if df[day][dtime] == '':
+                        if 'break' in df[1][dtime].lower():
+                            summary = df[1][dtime]
+                        else:
+                            summary = df[day][dtime - 1]
+                else:
+                    summary = df[day][dtime]
+                timing = df[0][dtime].split('to')
+                if timing[0].strip() >= '08:00' and timing[0].strip() < '12:00':
+                    period = 'am'
+                else:
+                    period = 'pm'
+                start_time = next(datefinder.find_dates(str(datetime.date(today)) + f' {timing[0]} {period}'))
+                if timing[1].strip() >= '08:00' and timing[1].strip() < '12:00':
+                    period = 'am'
+                else:
+                    period = 'pm'
+                end_time = next(datefinder.find_dates(str(datetime.date(today)) + f' {timing[1]} {period}'))
+                timezone = 'Asia/Kolkata'
+                event = {
+                    'summary':summary,
+                    'location':'PICT,Pune',
+                    'start':{
+                        'dateTime': start_time.isoformat(), 
+                        'timeZone': timezone
+                    },
+                    'end':{
+                        'dateTime':end_time.isoformat(),
+                        'timeZone':timezone
+                    }
                 }
-            }
-            event = service.events().insert(calendarId='primary',body=event).execute()
-            eventIds.append(event.get('id'))
-        print(f'{datetime.date(today)} done ')
-        today = today + timedelta(days=1)
-    print('Timetable Successfully uploaded to your Google Calendar')
-    return eventIds
+                event = self.service.events().insert(calendarId='primary',body=event).execute()
+                self.eventIds.append(event.get('id'))
+            today = today + timedelta(days=1)
+        print('Timetable Successfully uploaded to your Google Calendar')
 
-def delete_events(service,eventIds):
-    if isinstance(eventIds,str):
-        eventIds = [eventsIds]
-    for eventId in eventIds:
-        service.events().delete(calendarId='primary',eventId=eventId).execute()
-
-
+    def delete_events(self):
+        for eventId in tqdm(self.eventIds):
+            self.service.events().delete(calendarId='primary',eventId=eventId).execute()
+        self.eventIds = []
+        
+    def __len__(self):
+        return len(self.eventIds)
